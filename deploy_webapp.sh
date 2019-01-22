cd5e563113cabd7a9c3625865c385bc7d068f795
Frank Ji <frankji@ca.ibm.com>
2019-01-22 11:32:26 -0500

    deleting webapp project as it's now included in ngnix

--- deploy_webapp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker build --tag=liberty-demo-webapp liberty-demo-webapp
-docker run --network=demo_net --ip=172.25.3.5 -itd --name webapp -p 9080:9080 -p 9443:9443  liberty-demo-webapp
